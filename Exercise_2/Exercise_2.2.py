415dec7b9b68fca48a858c6f5acad278a7c7ac0c
HenryVag <146335816+HenryVag@users.noreply.github.com>
2024-01-16 12:25:43 +0200

    Working code

--- Exercise_2/Exercise_2.2.py
+++ Exercise_2/Exercise_2.2.py
@@ -15,13 +15,37 @@ class Coin:
 
     def toss_the_coin(self):
         if random.randint(0,1) == 0:
-            self.sideup = "Heads"
+            if self.toss_or_event() == True:
+                self.sideup = "Heads"
+            else:
+                self.random_event()
 
         else:
-            self.sideup = "Tails"
+            if self.toss_or_event() == True:
+                self.sideup = "Tails"
+            else: 
+                self.random_event()
 
     def get_sideup(self):
         return self.sideup
+    
+    def random_event(self):
+        eventnum = random.randint(0,2)
+        if eventnum == 0:
+            self.sideup = "Coin lands on the table upright"
+        elif eventnum == 1:
+            self.sideup = "Coin drops on the ground and disappearsin a rabbit hole"
+        else:
+            self.sideup = "Coin defies gravity and gets lost on a wormhole in space"
+
+
+    def toss_or_event(self):
+        if random.randint(0,1) == 0:
+            return True
+        
+        else:
+            self.random_event()
+        
     
 #Main function definition
     
@@ -33,7 +57,13 @@ def main():
     print("This side up:", my_coin.get_sideup())
     print("Tossing the coin...")
     my_coin.toss_the_coin()
-    print("Now this side is up:", my_coin.get_sideup())
+    
+    if my_coin.get_sideup() == "Heads":
+        print("Now this side is up:", my_coin.get_sideup())
+    elif my_coin.get_sideup() == "Tails":
+        print("Now this side is up:", my_coin.get_sideup())
+    else: 
+        print(my_coin.get_sideup())
 #Calling main function
     
 main()

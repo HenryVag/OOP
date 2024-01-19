d04afd929e7fd032f99ccf60387c384986b251e4
HenryVag <146335816+HenryVag@users.noreply.github.com>
2024-01-19 10:02:36 +0200

    2.8 and 2.9

--- Exercise_2/Exercise_2.8.py
+++ Exercise_2/Exercise_2.8.py
@@ -2,3 +2,28 @@
 #Author: Henry Våg
 #Description: Exercise 2.8
 
+class Checklist:
+
+    def __init__(self, header,entries):
+        self.header = str(header)
+        self.entries = list(entries)
+
+
+class Customer:
+
+    def __init__(self, id, balance, discount):
+        self.id = str(id)
+        self.balance = float(balance)
+        self.discount = int(discount)
+
+class Cable:
+
+    def __init__(self, model, length, max_speed, bidirectional):
+        self.model = str(model)
+        self.length = float(length)
+        self.max_speed = int(max_speed)
+        self.bidirectional = bool(bidirectional)
+
+
+
+

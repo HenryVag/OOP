e4487b6dfd7bd7ae9cf7ce14682ef055640c5812
HenryVag <146335816+HenryVag@users.noreply.github.com>
2024-01-19 09:35:47 +0200

    Working 2.7

--- Exercise_2/Exercise_2.7.py
+++ Exercise_2/Exercise_2.7.py
@@ -1,3 +1,16 @@
 #File name: Exercise_2.7
 #Author: Henry Våg
 #Description: Exercise 2.7
+
+class Manga:
+
+    def __init__(self, name, author, genre, release_year):
+
+        self.name = name
+        self.author = author
+        self.genre = genre
+        self.release_year = release_year
+
+berserk = Manga("Berserk","Miura", "Seinen", "1990")
+
+print(f"{berserk.author}: {berserk.name} ({berserk.release_year})")

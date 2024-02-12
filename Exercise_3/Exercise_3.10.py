3f4f4522c5d31fcbf8e33c16a4b80958cb3715e1
HenryVag <146335816+HenryVag@users.noreply.github.com>
2024-02-12 13:18:17 +0200

    Working 3.10???

--- Exercise_3/Exercise_3.10.py
+++ Exercise_3/Exercise_3.10.py
@@ -5,11 +5,46 @@
 
 class BankAccount:
 
-    def __init__(self, owner, account_number, balance):
-        self.owner = owner 
-        self.account_number = account_number
-        self.balance = balance
+    def __init__(self, owner: str, account_number: str, balance: float):
+        self.__owner = owner 
+        self.__account_number = account_number
+        self.__balance = balance
+
+
+    @staticmethod
+    def __service_charge():
+        amount = account.__balance * 0.01
+        account.__balance -= amount
+        
+    
+    @staticmethod
+    def deposit(amount: float):
+        account.__balance += amount
+        BankAccount.__service_charge()
+        print(f'Added ${amount} to account')
+    
+    @staticmethod
+    def withdraw(amount: float):
+        account.__balance -= amount
+        BankAccount.__service_charge()
+        print(f'Withdrew ${amount} from account')
+
+    @property
+    def balance(self):
+        return(self.__balance)
+    
+   
+
+
+
+
+account = BankAccount("Randy Riches","12345-6789",1000)
+account.withdraw(100)
+print(account.balance)
+account.deposit(100)
+print(account.balance)
     
 
-    def deposit(self, balance):
-        
+
+
+

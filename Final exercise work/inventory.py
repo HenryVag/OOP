d1edb7e2d005823728de727e2e3e5992a257bd16
HenryVag <henry.vag@edu.turkuamk.fi>
2024-04-04 13:12:39 +0300

    added handling in case title is not found/ not enough copies

--- Final exercise work/inventory.py
+++ Final exercise work/inventory.py
@@ -4,17 +4,32 @@ class Inventory:
 
     def __init__(self):
         self.games = {
-            "witcher3": Game("The Witcher 3", "RPG"),
-            "stardew valley": Game("Stardew Valley", "Farm RPG"),
+            1: Game("The Witcher 3", "RPG"),
+            2: Digitalgame("Stardew Valley", "Farm RPG"),
         }
 
     def subtract_game(self, title, amount):
+        game_found = False
         for game_key, game in self.games.items():
-            if game.title == title:
+            if game.title == title and game.qty >= int(amount):
                 game.qty -= int(amount)
-                print(game.qty)
-                
+                print(f"order completed! {game.qty} available copies of {game.title} left")
+                game_found = True
+            elif game.title == title and game.qty < int(amount):
+                print("not enough copies available")
+                game_found = True
+            
+        if not game_found:
+            print(f"{title} is not in our inventory")
+    
+    def add_game(self, game):
+        game_key = self.gen_key()
+        self.games[game_key] = game
 
+
+    def gen_key(self):
+        max_key = max(self.games)
+        return max_key + 1
 
 inv = Inventory()
 

3fe37121b3630c3b6ab88b59348f8465464df298
HenryVag <henry.vag@edu.turkuamk.fi>
2024-04-04 13:11:07 +0300

    added commands to main

--- Final exercise work/main.py
+++ Final exercise work/main.py
@@ -6,19 +6,28 @@ def main():
    while True:
     commands()
     command = input("command:")
-    execute_command(command)
+    if command == "0":
+        break
+    else:
+        execute_command(command)
 
 def commands():
     print("commands:")
     print("---------")
     print("0 - exit")
     print("1 - order")
+    print("2 - list games")
+    print("3 - add new game")
 
-
-def execute_commands(command):
-   if command = 0:
-    break
-
+def execute_command(command):
+    if command == "1":
+       gamemgr.order()
+    if command == "2":
+       gamemgr.list_games() 
+    if command == "3":
+       gamemgr.add_new_game()
+    
+    
 
 
 

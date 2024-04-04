dd474b789d8456c4c34baf8ad5818bf255cca775
HenryVag <henry.vag@edu.turkuamk.fi>
2024-04-04 13:01:35 +0300

    Finished game listing functionality

--- Final exercise work/game_manager.py
+++ Final exercise work/game_manager.py
@@ -1,5 +1,5 @@
 from inventory import Inventory
-
+from game import Game, Digitalgame
 
 
 class GameManager:
@@ -13,8 +13,21 @@ class GameManager:
         amount = input("How many copies?:")
         self.inv.subtract_game(title, amount)
 
+    def list_games(self):
+        for game_key, game in self.inv.games.items():
+            print(game.title)
 
-
+    def add_new_game(self):
+        print("-----------")
+        title = input("title:")
+        genre = input("genre:")
+        type = input("digital y/n?:")
+        if type == "y":
+            game = Digitalgame(title, genre)
+        if type == "n":
+            game = Game(title, genre)
+            
+        self.inv.add_game(game)
 
 
 gamemgr = GameManager(Inventory())
